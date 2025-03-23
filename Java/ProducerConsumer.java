fb20a77ca46a84f94c0f41bb4ead17ed728f631b
ArunRathaur1 <arunrathaur92.6@gmail.com>
2025-03-23 17:00:00 +0530

    updated sturcture

--- Java/ProducerConsumer.java
+++ Java/ProducerConsumer.java
@@ -1,68 +1,71 @@
-public class ProducerConsumer {
-    static int size = 5;
-    static int buffer[] = new int[size];
-    static int count = 0;
-    static int in = 0, out = 0;
+class procuderconsumer {
+    static int [] buffer =new int[5];
+    static int buffersize=5;
+    static int count=0;
+    static int in=0;
+    static int out=0;
+    static class producer extends Thread{
+        public void run(){
+            int value=0;
+            int x=0;
+            while(x<5){
+                synchronized(buffer){
+                    while(count==buffersize){
+                        try{
+                            buffer.wait();
+                        }
+                        catch(Exception e){
 
-    // Synchronization lock
-    static final Object lock = new Object();
-
-    // Producer thread
-    public class Producer extends Thread {
-        public void run() {
-            int value = 0;
-            while (true) {
-                synchronized (lock) {
-                    // Wait if the buffer is full
-                    while (count == size) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
                         }
                     }
-                    // Produce an item
-                    buffer[in] = value;
-                    System.out.println("Value Produced: " + value);
+                    System.out.println("Producer produced item "+ value);
+                    in=(in+1)%buffersize;
+                    buffer[in]=value;
                     value++;
-                    in = (in + 1) % size;
                     count++;
-                    lock.notifyAll(); // Notify waiting threads
+                    buffer.notifyAll();
+                    try{
+                        Thread.sleep(500);
+                    }
+                    catch(Exception e){
+
+                    }
                 }
+                x++;
             }
         }
     }
-
-    // Consumer thread
-    public class Consumer extends Thread {
-        public void run() {
-            while (true) {
-                synchronized (lock) {
-                    // Wait if the buffer is empty
-                    while (count == 0) {
+    static class consumer extends Thread{
+        public void run(){
+            int x=0;
+            while(x<5){
+                synchronized(buffer){
+                    while(count==0){
                         try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            buffer.wait();
+                        } catch (Exception e) {
+                           
                         }
                     }
-                    // Consume an item
-                    int consumedValue = buffer[out];
-                    System.out.println("Value Consumed: " + consumedValue);
-                    out = (out + 1) % size;
+                    System.out.println("Consumer consumed "+buffer[out]);
+                    out=(out+1)%buffersize;
                     count--;
-                    lock.notifyAll(); // Notify waiting threads
+                    buffer.notifyAll();
+                    try{
+                        Thread.sleep(500);
+                    }
+                    catch(Exception e){
+
+                    }
                 }
+                x++;
             }
         }
     }
-
-    public static void main(String[] args) {
-        ProducerConsumer obj = new ProducerConsumer();
-        Producer producer = obj.new Producer();
-        Consumer consumer = obj.new Consumer();
-
-        producer.start();
-        consumer.start();
+    public static void main(String args[]){
+        producer ob=new producer();
+        consumer ob1=new consumer();
+        ob.start();
+        ob1.start();
     }
 }
